9529d2d0f547807c35502cafe4e0dd92c622cf5a
Alireza Bazargani <arbazargani1998@gmail.com>
2021-08-22 15:18:04 +0430

    Minor update - newspaper old archive v3-no-pagination-keys chunk count changed\.

--- app/Http/Controllers/NewspaperController.php
+++ app/Http/Controllers/NewspaperController.php
@@ -9,7 +9,7 @@ class NewspaperController extends Controller
     public function ListDir() {
         $dir = 'repository';
         $versions = scandir($dir, 1);
-        $versions = array_chunk($versions, 1);
+        $versions = array_chunk($versions, 10);
         $versions = isset($_GET['page']) ? $versions[$_GET['page']] : $versions[0];
         return $versions;
     }
