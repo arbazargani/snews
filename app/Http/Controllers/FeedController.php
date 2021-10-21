f8955860514583617497d2aee64b992a9c1175a4
Alireza Bazargani <arbazargani1998@gmail.com>
2021-10-21 10:41:23 +0330

    Minor update - RSS dataset bug fixed.

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use Carbon\Carbon;
 
 class FeedController extends Controller
 {
     public function Index() {
         // $articles = Article::where('state', '=', 1)->get();
-        $articles = Article::where('state', '=', 1)->orderBy('id', 'DESC')->limit(15)->get();
+        $articles = Article::where('state', '=', 1)
+                            ->where('created_at','<=', Carbon::now())
+                            ->orderBy('id', 'DESC')->limit(15)->get();
         $lastModified = $articles->first();
         return response()
         ->view('public.feed.index', compact(['articles', 'lastModified']))
